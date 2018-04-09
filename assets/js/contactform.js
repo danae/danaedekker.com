12c0798c67716798fa75fa0ac3fa34394a0b2b90
dengsn <info@dengsn.com>
2018-04-09 13:17:17 +0200

    Version 2018-04-09 13:17

---
+++ assets/js/contactform.js
@@ -0,0 +1,44 @@
+$(function() {
+  // Get the form
+  var form = $('#contactForm');
+  
+  // Get the messages div
+  var formMessages = $('#contactMessages');
+  
+  // Set up an event listener for the form
+  $(form).submit(function(e) {
+    // Stop the browser from submitting the form
+    e.preventDefault();
+    
+    // Serialize the form data
+    var formData = $(form).serialize();
+    
+    // Submit the form using AJAX
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    }).done(function(response) {
+      // Create an alert with the success message
+      var alert = document.createElement('div');
+      $(alert).addClass('alert alert-success');
+      $(alert).text(response.message);
+      
+      // Add the alert
+      $(formMessages).html('').append(alert);
+
+      // Clear the form
+      $('#name').val('');
+      $('#email').val('');
+      $('#message').val('');
+    }).fail(function(data) {
+      // Create an alert with the error message
+      var alert = document.createElement('div');
+      $(alert).addClass('alert alert-danger');
+      $(alert).text(data.responseJSON.error);
+      
+      // Add the alert
+      $(formMessages).html('').append(alert);
+    });
+  });
+});
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+require("vendor/autoload.php");
+require("settings.php");
+
+use Feather\Feather;
+use Feather\Page;
+use Soundcloud\Soundcloud;
+
+// Create a SoundCloud wrapper
+$soundcloud = new Soundcloud($soundcloudId,$soundcloudSecret);
+
+// Create the context
+$feather = new Feather('templates');
+
+// Add pages
+$feather->addPage(new Page('compositions'));
+$feather->addPage(new Page('projects'));
+$feather->addPage(new Page('about'));
+$feather->addPage(new Page('contact'));
+
+// Set the error pages
+$feather->setErrorPage(new Page('500'));
+$feather->setNotFoundPage(new Page('404'));
+
+// Run!
+$feather->run([
+  'root' => Feather::rootPath(),
+  'assets' => Feather::rootPath() . '/assets',
+  'soundcloud' => $soundcloud,
+  'links' => [
+    'email' => 'mailto:info@dennisdekker.art',
+    'spotify' => 'https://open.spotify.com/artist/26zydGirRGiAVUaWRR0Wvt?si=mp5rMhmAR5qQ11ypBLJevw',
+    'soundcloud' => 'https://soundcloud.com/purplelum',
+    'github' => 'https://github.com/dengsn',
+    'facebook' => 'https://www.facebook.com/purplelum'
+  ],
+  'projects' => [
+    'our_little_planet' => 'http://perspectiveworks.nl/OLP/public/',
+    'blue_moon' => [    
+      'spotify' => 'https://open.spotify.com/album/759u3tjG0t3Fkkycxd9SNC?si=fEArNvEiRMOxaILlXakgJg',
+      'deezer' => 'https://www.deezer.com/nl/album/59655342',
+      'soundcloud' => 'https://soundcloud.com/purplelum/sets/blue-moon'
+    ]
+  ]
+]);
