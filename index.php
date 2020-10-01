a5930c61e53729a482d18e4dd51df44d63ef6348
Danae Dekker <info@danaedekker.com>
2020-10-01 18:07:09 +0200

    Added Steam and Itch.io links for IW

--- index.php
+++ index.php
@@ -43,7 +43,9 @@ $feather->context = [
   'projects' => [
     'invisible_wings' => [
       'embed' => 'https://www.youtube.com/embed/UB10AQidnxM',
-      'link' => 'https://invisiblewingsgame.com/'
+      'link' => 'https://invisiblewingsgame.com/',
+      'steam' => 'https://store.steampowered.com/app/1419380/Invisible_Wings/',
+      'itch' => 'https://audune.itch.io/invisiblewings',
     ],
     'ocarime' => [
       'image' => $feather->base_path . '/assets/images/splash_ocarime.png',
