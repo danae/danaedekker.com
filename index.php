63080de4e0defd03d3542812f74a460c62856fb4
Danae Dekker <info@danaedekker.com>
2021-04-29 15:29:11 +0200

    Added showreel to home page, reorganized links, changed some texts

--- index.php
+++ index.php
@@ -32,19 +32,30 @@ $feather->context = [
   'assets' => $feather->base_path . '/assets',
   'soundcloud' => $soundcloud,
   'links' => [
-    'resume' => $feather->base_path . '/publications/Danae_Dekker_CV_2021.pdf',
-    'commissions_tos' => $feather->base_path . '/publications/Personal_Music_Comissions_TOS.pdf',
-    'dutch_tax_exemptions' => 'https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/zakelijk/btw/tarieven_en_vrijstellingen/vrijstellingen/vrijstelling_voor_componisten_schrijvers_cartoonisten_en_journalisten',
-    'linkedin' => 'https://linkedin.com/in/danaedekker/',
-    'facebook' => 'https://www.facebook.com/danaedekkergameaudio',
+    'spotify_danaedekker' => 'https://open.spotify.com/artist/0bta46UUaupzYcrrhG3yRQ',
+    'spotify_purplelum' => 'https://open.spotify.com/artist/26zydGirRGiAVUaWRR0Wvt',
+    'deezer_danaedekker' => 'https://www.deezer.com/nl/artist/124460692',
+    'deezer_purplelum' => 'https://www.deezer.com/nl/artist/14398631',
     'twitter' => 'https://twitter.com/da___nae',
+    'facebook' => 'https://www.facebook.com/danaedekkergameaudio',
+    'linkedin' => 'https://linkedin.com/in/danaedekker/',
     'linktree' => 'https://danae.link/'
+  ],
+  'home' => [
+    'showreel_embed' => 'https://www.youtube.com/embed/Ox1CHWTb3Vk'
+  ],
+  'compositions' => [
+    'ocean_of_stars' => 'https://soundcloud.com/purplelum/ocean-of-stars',
+    'entanglement' => 'https://soundcloud.com/purplelum/entanglement',
+    'celestine' => 'https://soundcloud.com/purplelum/celestine',
+    'once_in_a_blue_moon' => 'https://soundcloud.com/purplelum/once-in-a-blue-moon-2020-remastered',
+    'we_in_heaven' => 'https://soundcloud.com/purplelum/we-in-heaven-2020-remix'
   ],
   'projects' => [
     'invisible_wings' => [
       'square' => $feather->base_path . '/assets/images/square_invisible_wings.png',
       'embed' => 'https://www.youtube.com/embed/Zn5NWS1Zm7Q',
-      'link' => 'https://invisiblewingsgame.com/',
+      'website' => 'https://invisiblewingsgame.com/',
       'steam' => 'https://store.steampowered.com/app/1419380/Invisible_Wings/',
       'itch' => 'https://audune.itch.io/invisiblewings'
     ],
@@ -95,6 +106,14 @@ $feather->context = [
       'google' => 'https://music.youtube.com/playlist?list=OLAK5uy_nuVhWZ0eLB_pLiHnvgAOiQlOZLIto_Vxw',
       'soundcloud' => 'https://soundcloud.com/purplelum/sets/blue-moon'
     ]
+  ],
+  'commissions' => [
+    'tos' => $feather->base_path . '/publications/Personal_Music_Comissions_TOS.pdf',
+    'dutch_tax_exemptions' => 'https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/zakelijk/btw/tarieven_en_vrijstellingen/vrijstellingen/vrijstelling_voor_componisten_schrijvers_cartoonisten_en_journalisten',
+  ],
+  'about' => [
+    'thesis' => $feather->base_path . '/publications/Nonlinear_music_design_in_narrative_games.pdf',
+    'resume' => $feather->base_path . '/publications/Danae_Dekker_CV_2021.pdf'
   ]
 ];
 
